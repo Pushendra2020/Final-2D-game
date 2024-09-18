25f21ceed2391f11e4b18035af95874a3a54dfdd
CodProdigy <165462522+CodProdigy@users.noreply.github.com>
2024-09-18 20:56:58 +0530

    added invulnerability frames to player health system

--- Assets/Scripts/Health/Health.cs
+++ Assets/Scripts/Health/Health.cs
@@ -1,16 +1,25 @@
 using UnityEngine;
+using System.Collections;
 
 public class Health : MonoBehaviour // Change the class name to 'Health'
 {
+    [Header ("Health")]
     [SerializeField] private float startingHealth;
     public float currentHealth { get; private set; }
     private Animator anim;
     private bool dead;
 
+
+    [Header ("iFrames")]
+    [SerializeField] private float iFramesDuration;
+    [SerializeField] private float numberofFlashes;
+    private SpriteRenderer spriteRend; 
+
     private void Awake()
     {
         currentHealth = startingHealth;
         anim = GetComponent<Animator>();
+        spriteRend = GetComponent<SpriteRenderer>();
     }
 
     public void TakeDamage(float _damage)
@@ -19,6 +28,7 @@ public class Health : MonoBehaviour // Change the class name to 'Health'
         if (currentHealth > 0)
         {
             anim.SetTrigger("hurt");
+            StartCoroutine(Invulnerability());
         }
         else
         {
@@ -37,5 +47,17 @@ public class Health : MonoBehaviour // Change the class name to 'Health'
         currentHealth = Mathf.Clamp(currentHealth + _value, 0, startingHealth);
     }
 
+    private IEnumerator Invulnerability()
+    {
+        Physics2D.IgnoreLayerCollision(10, 11, true);
+        for (int i = 0; i < numberofFlashes; i++)
+        {
+            spriteRend.color = new Color(1,0,0, 0.5f);
+            yield return new WaitForSeconds(iFramesDuration / (numberofFlashes) * 2);
+            spriteRend.color = Color.white;
+            yield return new WaitForSeconds(iFramesDuration / (numberofFlashes) * 2);
+        }
+        Physics2D.IgnoreLayerCollision(10, 11, false);
+    }
 }
 
